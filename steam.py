1b60c7761626456646dc865f3f93f55f9ba6b183
mutlaq11 <141351115+mutlaq11@users.noreply.github.com>
2023-08-03 20:00:23 +0300

    steam.py

--- steam.py
+++ steam.py
@@ -14,7 +14,11 @@ sequence_to_classify = st.text_input("Enter the sequence to classify:")
 if st.button("Classify"):
     if sequence_to_classify:
         candidate_labels = ["transportations", "food", "bathroom", "guidance"]
-        output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
-        st.write("Classification:", output['labels'][0])
+        try:
+            output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
+            st.write("Classification:", output['labels'][0])
+        except Exception as e:
+            st.error("An error occurred during classification.")
+            st.error(str(e))
     else:
         st.warning("Please enter a sequence to classify.")
