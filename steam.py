8475584b862afae7a41b3b212617a77620955054
mutlaq11 <141351115+mutlaq11@users.noreply.github.com>
2023-08-03 19:31:13 +0300

    Add files via upload

---
+++ steam.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+
+# Load the model
+model_name = "MoritzLaurer/mDeBERTa-v3-base-mnli-xnli"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+classifier = pipeline("zero-shot-classification", model=model, tokenizer=tokenizer)
+
+st.title("Sequence Classification")
+
+# Input
+sequence_to_classify = st.text_input("Enter the sequence to classify:")
+if st.button("Classify"):
+    if sequence_to_classify:
+        candidate_labels = ["transportations", "food", "bathroom", "guidance"]
+        output = classifier(sequence_to_classify, candidate_labels, multi_label=False)
+        st.write("Classification:", output['labels'][0])
+    else:
+        st.warning("Please enter a sequence to classify.")
